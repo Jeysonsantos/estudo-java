a55adb659f750bef3e3e3db97876ee4df74f0044
Jeyson <jeyson-santos@hotmail.com>
2023-02-09 09:22:08 -0300

    Atualizando sistema

--- projeto-java/src/App.java
+++ projeto-java/src/App.java
@@ -1,11 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
-/**
- *
- * @author vinic
-*/
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Map;
@@ -19,7 +12,7 @@ public class App {
         Map<String,ArrayList<String>> dici = new HashMap<>();
         ArrayList<String>notas = new ArrayList<String>();
 
-        System.out.println("Seja bem vindo !");
+        System.out.println("Seja bem vindo ao sistema de cadastro!");
         while(true){
             System.out.println("Opções:"
                 + "\n 1.Matricular um aluno no sistema."
@@ -28,10 +21,11 @@ public class App {
                 + "\n 4.Adicionar Faltas."
                 + "\n 0.Sair");
             Scanner opt = new Scanner(System.in);
-            int op = opt.nextInt();
-            if(op == 0){break;}
+
+            String op = opt.nextLine();
+            if(op == "0"){break;}
         
-        if(op == 1)
+        if(op == "1")
         {
             Scanner matricula = new Scanner(System.in);
             System.out.println("Digite o nome do aluno:");
@@ -47,9 +41,10 @@ public class App {
             dados.add(mat);
             dados.add(disciplina);
             dici.put(nome,dados);
-        }else if(op==2){
+        }else if(op=="2"){
             System.out.println();
-            System.out.println("Selecione um aluno:");
+            System.out.println("--Selecione um aluno--");
+            System.out.println("Digite um inteiro correspondente:");
             System.out.println();
             for(int i=0;i<nome_Alunos.size();i++){
                 System.out.println(i+")"+ nome_Alunos.get(i));
@@ -58,13 +53,14 @@ public class App {
             System.out.println();
             System.out.println();
             System.out.println("CPF:" + dici.get(nome_Alunos.get(op_nome)).get(0));
-            System.out.println("MATRICULA:" + dici.get(nome_Alunos.get(op_nome)).get(1));
+            System.out.println("MATRÍCULA:" + dici.get(nome_Alunos.get(op_nome)).get(1));
             System.out.println("DISCIPLINA MATRICULADA:" + dici.get(nome_Alunos.get(op_nome)).get(2));
             System.out.println();
             System.out.println();
-        }else if(op==3){
+        }else if(op=="3"){
             System.out.println();
-            System.out.println("Selecione um aluno:");
+            System.out.println("--Selecione um aluno--");
+            System.out.println("Digite um inteiro correspondente:");
             System.out.println();
             for(int i=0;i<nome_Alunos.size();i++){
                 System.out.println(i+")"+ nome_Alunos.get(i));
@@ -76,20 +72,28 @@ public class App {
             String aux_disciplina = dici.get(nome_Alunos.get(op_nome2)).get(2);
             dici.get(nome_Alunos.get(op_nome2)).set(2,aux_disciplina+" \n NOTA :  "+"("+nota+")");
             
-        }else if(op==4){
+        }else if(op=="4"){
             System.out.println();
-            System.out.println("Selecione um aluno:");
+            System.out.println("--Selecione um aluno--");
+            System.out.println("Digite um inteiro correspondente:");
             System.out.println();
             for(int i=0;i<nome_Alunos.size();i++){
                 System.out.println(i+")"+ nome_Alunos.get(i));
             }
             int op_nome3 = opt.nextInt();opt.nextLine();
             System.out.println();
-            System.out.println("Digite a quantidade de faltas:");
+            System.out.println("Digite a quantidade de faltas(Int):");
             String num_faltas = opt.nextLine();
             String aux_disciplina = dici.get(nome_Alunos.get(op_nome3)).get(2);
             dici.get(nome_Alunos.get(op_nome3)).set(2,aux_disciplina+" \n"+"FALTAS : ("+num_faltas+")");
+        }else{
+            System.out.println();
+            System.out.println("ERROR");
+            System.out.println("Digite uma opção válida!");
+            System.out.println();
+
         }
+
                 
         }
     }
