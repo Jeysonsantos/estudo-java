82f9ebf9e66c15331bf58050d67ff462c728580a
Jeyson <jeyson-santos@hotmail.com>
2023-02-09 10:09:43 -0300

    Atualizando entrada de dados
    Corrigindo possíveis erros com entradas não esperadas.

--- projeto-java/src/App.java
+++ projeto-java/src/App.java
@@ -21,81 +21,92 @@ public class App {
                 + "\n 4.Adicionar Faltas."
                 + "\n 0.Sair");
             Scanner opt = new Scanner(System.in);
-
             String op = opt.nextLine();
-            if(op == "0"){break;}
-        
-        if(Integer.parseInt(op) == 1)
-        {
-            Scanner matricula = new Scanner(System.in);
-            System.out.println("Digite o nome do aluno:");
-            String nome = matricula.nextLine();
-            System.out.println("Digite o CPF");
-            String cpf = matricula.nextLine();
-            System.out.println("Digite o numero de matricula:");
-            String mat = matricula.nextLine();
-            System.out.println("Digite o nome da disciplina:");
-            String disciplina = matricula.nextLine();
-            nome_Alunos.add(nome);
-            dados.add(cpf);
-            dados.add(mat);
-            dados.add(disciplina);
-            dici.put(nome,dados);
-        }else if(Integer.parseInt(op) == 2){
-            System.out.println();
-            System.out.println("--Selecione um aluno--");
-            System.out.println("Digite um inteiro correspondente:");
-            System.out.println();
-            for(int i=0;i<nome_Alunos.size();i++){
-                System.out.println(i+")"+ nome_Alunos.get(i));
-            }
-            System.out.println((nome_Alunos.size()) +")" + "VOLTAR");
-            int op_nome = opt.nextInt();
-            System.out.println();
-            System.out.println();
-            System.out.println("CPF:" + dici.get(nome_Alunos.get(op_nome)).get(0));
-            System.out.println("MATRÍCULA:" + dici.get(nome_Alunos.get(op_nome)).get(1));
-            System.out.println("DISCIPLINA MATRICULADA:" + dici.get(nome_Alunos.get(op_nome)).get(2));
-            System.out.println();
-            System.out.println();
-        }else if(Integer.parseInt(op) == 3){
-            System.out.println();
-            System.out.println("--Selecione um aluno--");
-            System.out.println("Digite um inteiro correspondente:");
-            System.out.println();
-            for(int i=0;i<nome_Alunos.size();i++){
-                System.out.println(i+")"+ nome_Alunos.get(i));
-            }
-            int op_nome2 = opt.nextInt();opt.nextLine();
-            System.out.println();
-            System.out.println("Digite a nota:");
-            String nota = opt.nextLine();
-            String aux_disciplina = dici.get(nome_Alunos.get(op_nome2)).get(2);
-            dici.get(nome_Alunos.get(op_nome2)).set(2,aux_disciplina+" \n NOTA :  "+"("+nota+")");
-            
-        }else if(Integer.parseInt(op) == 4){
-            System.out.println();
-            System.out.println("--Selecione um aluno--");
-            System.out.println("Digite um inteiro correspondente:");
-            System.out.println();
-            for(int i=0;i<nome_Alunos.size();i++){
-                System.out.println(i+")"+ nome_Alunos.get(i));
-            }
-            int op_nome3 = opt.nextInt();opt.nextLine();
-            System.out.println();
-            System.out.println("Digite a quantidade de faltas(Int):");
-            String num_faltas = opt.nextLine();
-            String aux_disciplina = dici.get(nome_Alunos.get(op_nome3)).get(2);
-            dici.get(nome_Alunos.get(op_nome3)).set(2,aux_disciplina+" \n"+"FALTAS : ("+num_faltas+")");
-        }else{
-            System.out.println();
-            System.out.println("ERROR");
-            System.out.println("Digite uma opção válida!");
-            System.out.println();
-
-        }
-
+        if(isInteger(op)){
+            if(Integer.parseInt(op) == 1)
+            {
+                Scanner matricula = new Scanner(System.in);
+                System.out.println("Digite o nome do aluno:");
+                String nome = matricula.nextLine();
+                System.out.println("Digite o CPF");
+                String cpf = matricula.nextLine();
+                System.out.println("Digite o numero de matricula:");
+                String mat = matricula.nextLine();
+                System.out.println("Digite o nome da disciplina:");
+                String disciplina = matricula.nextLine();
+                nome_Alunos.add(nome);
+                dados.add(cpf);
+                dados.add(mat);
+                dados.add(disciplina);
+                dici.put(nome,dados);
+            }else if(Integer.parseInt(op) == 2){
+                System.out.println();
+                System.out.println("--Selecione um aluno--");
+                System.out.println("Digite um inteiro correspondente:");
+                System.out.println();
+                for(int i=0;i<nome_Alunos.size();i++){
+                    System.out.println(i+")"+ nome_Alunos.get(i));
+                }
+                //System.out.println((nome_Alunos.size()) +")" + "VOLTAR");
+                int op_nome = opt.nextInt();
+                System.out.println();
+                System.out.println();
+                System.out.println("CPF:" + dici.get(nome_Alunos.get(op_nome)).get(0));
+                System.out.println("MATRÍCULA:" + dici.get(nome_Alunos.get(op_nome)).get(1));
+                System.out.println("DISCIPLINA MATRICULADA:" + dici.get(nome_Alunos.get(op_nome)).get(2));
+                System.out.println();
+                System.out.println();
+            }else if(Integer.parseInt(op) == 3){
+                System.out.println();
+                System.out.println("--Selecione um aluno--");
+                System.out.println("Digite um inteiro correspondente:");
+                System.out.println();
+                for(int i=0;i<nome_Alunos.size();i++){
+                    System.out.println(i+")"+ nome_Alunos.get(i));
+                }
+                int op_nome2 = opt.nextInt();opt.nextLine();
+                System.out.println();
+                System.out.println("Digite a nota:");
+                String nota = opt.nextLine();
+                String aux_disciplina = dici.get(nome_Alunos.get(op_nome2)).get(2);
+                dici.get(nome_Alunos.get(op_nome2)).set(2,aux_disciplina+" \n NOTA :  "+"("+nota+")");
                 
-        }
+            }else if(Integer.parseInt(op) == 4){
+                System.out.println();
+                System.out.println("--Selecione um aluno--");
+                System.out.println("Digite um inteiro correspondente:");
+                System.out.println();
+                for(int i=0;i<nome_Alunos.size();i++){
+                    System.out.println(i+")"+ nome_Alunos.get(i));
+                }
+                int op_nome3 = opt.nextInt();opt.nextLine();
+                System.out.println();
+                System.out.println("Digite a quantidade de faltas(Int):");
+                String num_faltas = opt.nextLine();
+                String aux_disciplina = dici.get(nome_Alunos.get(op_nome3)).get(2);
+                dici.get(nome_Alunos.get(op_nome3)).set(2,aux_disciplina+" \n"+"FALTAS : ("+num_faltas+")");
+            }else if(Integer.parseInt(op) == 0){
+                break;
+            }
+            else{
+                System.out.println();
+                System.out.println("-----------ERROR------------");
+                System.out.println();
+                System.out.println();
+                System.out.println("Digite uma opção válida!");
+                System.out.println();
+                System.out.println();
+
+            }
+    }else{
+        System.out.println();
+        System.out.println();
+        System.out.println("------------Opção Inválida!------------");
+        System.out.println();
+    }
+    }
+    }
+    private static boolean isInteger(String str) {
+        return str != null && str.matches("[0-9]*");
     }
 }
